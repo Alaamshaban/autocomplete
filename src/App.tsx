55dab63a944124318869dbd4f4337fd17ffee7f0
Alaamshaban <alaamohamedshaban@yahoo.com>
2023-06-17 18:45:32 +0300

    add fetch in useEffect hook to fetch data from an api

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,10 @@ import Autocomplete from "./components/autocomplete";
 
 
 const App = () => {
-  const options = [
-    { id: 1, value: "Apple" },
-    { id: 2, value: "Banana" },
-    { id: 3, value: "Orange" },
-  ]
-
-  const [suggestions, setSuggestions] = useState(options);
-  const [value, setValue] = useState("");
-
-  const completeMethod = (value) => {
-    setValue(value)
-    value !== '' ? setSuggestions(options.filter(option => option.value.toLowerCase().includes(value.toLowerCase()))) : setSuggestions(options)
-  };
-
-
   return (
     <div>
       <Autocomplete
-        suggestions={suggestions}
-        getSuggestions={completeMethod}
-        value={value}
+        url='http://universities.hipolabs.com/search?country=United+States'
       />
     </div>
   );
